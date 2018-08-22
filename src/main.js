faa2f57546cac1ae884b347295612459fe1d198a
webzhangbing <273302971@qq.com>
2018-08-22 21:40:36 +0800

    add

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import 'normalize.css/normalize.css'// A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import './assets/iconfont.css'
 import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
 import { sync } from 'vuex-router-sync'
 import '@/styles/index.scss' // global css
--- src/views/recommend/list.vue
+++ src/views/recommend/list.vue
@@ -10,19 +10,19 @@
           el-tabs(type="border-card" v-model="activeTab")
             el-tab-pane(name="1")
               span(slot="label") 
-                img(src="../../assets/commend.svg")
+                i.icon.icon-zonghe
                 span.text 综合排序
             el-tab-pane(name="2")
               span(slot="label") 
-                img(src="../../assets/commend1.svg")
+                i.icon.icon-canting
                 span.text 餐厅
             el-tab-pane(name="3")
               span(slot="label") 
-                img(src="../../assets/commend2.svg")
+                i.icon.icon-xingzhuang
                 span.text 酒吧
             el-tab-pane(name="4")
               span(slot="label") 
-                img.yedian(src="../../assets/commend3.svg")
+                i.icon.icon-caozuoyedian
                 span.text 夜店
         .content-bottom
           .commend-list(v-for="item in 9")
@@ -42,6 +42,20 @@
                 img(src="../../assets/canting.svg")
               .desc After barely four months of operation, Peruvian restaurant Ekeko has closed. The announcement was made by chef Carlos Sotomayor on…
       .content-right
+        .newTrends-t
+          .trendsButton New Trends
+          .trends-list(v-for="item in 5")
+            .img-box
+              img(src="../../assets/footerAd.jpg")
+            .content-info
+              h3 New Peruvian Spot Ekeko Closes Abruptly Overnight
+              .date 2018/04/29
+              el-button 查看
+        .adv-b
+          .adv_title ADVERTISEMENT
+          .adv_content
+            img(src="../../assets/userAdpic.jpg")
+            img(src="../../assets/userAdpic.jpg")
 </template>
 
 <script>
@@ -77,9 +91,10 @@
           background #29ABE2
     .recommend-content
       display flex
+      margin-top 13px
       .content-left
         width 861px
-        margin 13px 13px 20px 0
+        margin 0 13px 20px 0
         .nav-top
           text-align center
           height 90px
@@ -98,9 +113,13 @@
             margin 0 auto
             line-height 20px
             height 20px
-          img
-            display block
-            margin 20px auto 7px
+          i.icon
+            font-size 26px
+            margin-top 13px
+            display inline-block
+          i.icon.icon-caozuoyedian
+            font-size 17px
+            margin-bottom 3px
           .yedian
             margin 10px auto 12px !important
           .el-tabs
@@ -220,4 +239,74 @@
                 color #8c8c8c
       .content-right
         flex 1
+        .newTrends-t
+          padding 6px 9px 3px
+          background #fff
+          .trendsButton
+            height 80px
+            background #014266
+            font-family Oswald
+            font-size 26px
+            font-weight bold
+            line-height 80px
+            text-align center
+            color #ffffff
+          .trends-list
+            padding 16px 0 22px 0
+            display flex
+            border-bottom 2px solid rgba(191, 191, 191, 0.3)
+            .img-box
+              width 118px
+              height 118px
+              margin-right 22px
+              img 
+                width 100%
+                height 100%
+            .content-info
+              flex 1
+              h3 
+                font-family Oswald
+                font-size 14px
+                line-height 18px
+                letter-spacing 0.4px
+                text-align left
+                color #000000
+                margin-bottom 14px
+              .date
+                height 17px
+                font-family PingFangSC
+                font-size 12px
+                font-weight 500
+                line-height 17px
+                letter-spacing 0.4px
+                text-align left
+                color #bfbfbf
+                margin-bottom 18px
+              .el-button
+                border none
+                height 33px
+                line-height 33px
+                font-size 12px
+                color #c7c7cc
+                background #f6f6f6
+                padding 0 30px
+              .el-button:focus, .el-button:hover
+                border-color #f6f6f6
+        .adv-b
+          .adv_title
+            height 64px
+            line-height 64px
+            font-family Helvetica
+            font-size 14px
+            letter-spacing 0.4px
+            text-align center
+            color #c7c7cc
+          .adv_content
+            font-size 0
+            text-align center
+            img 
+              width 300px
+              height 300px
+              display inline-block
+              margin-bottom 20px
 </style>
